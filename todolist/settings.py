19e30e95e140b9778654ce2accde12a88128012b
almyk <hansentommy92@gmail.com>
2017-11-01 00:45:00 +0900

    Update allowed hosts

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^5zu6ms!&==fj#yzerjcc&2j_iu3q!pi3di*o9791z+47y_$fi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.79.161.1']
 
 
 # Application definition
