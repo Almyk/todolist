6559f80a1b808e323a412a67001b9586cfcf9b89
almyk <hansentommy92@gmail.com>
2017-11-03 03:32:03 +0900

    change urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -17,9 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    url(r'^$', include('todos.urls')),
+    url(r'^$', RedirectView.as_view(url='todos/')),
     url(r'^todos/', include('todos.urls')),
     url(r'^admin/', admin.site.urls),
 ]
